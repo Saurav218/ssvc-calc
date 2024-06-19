48a747ef3893e33e7dc642ea01ebe78e25235deb
Saurav Brahma <sbrahma@yahoo-corp.jp>
2024-06-19 19:28:11 +0900

    fix: header
    add three decision tree links

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,37 +4,63 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Link from 'next/link';
+import AdbIcon from '@mui/icons-material/Adb';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function ButtonAppBar() {
+const pages = ['Supplier', 'Deployer', 'Coordinator'];
+
+function ResponsiveAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             SSVC Calculator
           </Typography>
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Link href={`/calculator/${page.toLowerCase()}`} passHref>
+                  <Typography textAlign="center" variant="button" color="inherit">
+                    {page}
+                  </Typography>
+                </Link>
+              </Button>
+            ))}
+          </Box>
+
           <IconButton
-                    color="inherit"
-                    aria-label="GitHub"
-                    onClick={() => window.open("https://mui.com/material-ui/api/icon-button/", "_blank")}
-                >
-                    <GitHubIcon />
-            </IconButton>
+            color="inherit"
+            aria-label="GitHub"
+            onClick={() => window.open("https://mui.com/material-ui/api/icon-button/", "_blank")}
+          >
+            <GitHubIcon />
+          </IconButton>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 }
+export default ResponsiveAppBar;
